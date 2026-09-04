c0ec38554538a79e226e0702bacd990a5ba4b714
Julie Ann Noda <julieannnoda@gmail.com>
2026-09-04 19:56:55 +0800

    Update users

--- app/config/routes.php
+++ app/config/routes.php
@@ -44,4 +44,5 @@ defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 */
 /** @var object $router **/
 
-$router->get('/', 'Welcome::index');
+$router->get('/', 'UserController::showUsers');
+$router->get('/users', 'UserController::showUsers');
